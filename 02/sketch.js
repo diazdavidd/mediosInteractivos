f4232ff369efd9150358af51eb98ee9fdd7dd34b
diazdavidd <42588561+diazdavidd@users.noreply.github.com>
2018-08-28 12:58:51 -0500

    Add files via upload

---
+++ 02/sketch.js
@@ -0,0 +1,313 @@
+//Variables
+//Triangulos inferiores
+var x1 = 15;
+var y1 = 15;
+var x2 = 25;
+var y2 = 25;
+//Triangulos superiores
+var x3 = 35;
+var y3 = 15;
+var x4 = 45;
+var y4 = 25;
+
+function setup() 
+{
+  createCanvas(600, 600);
+  background(220);
+  fill(0);
+noStroke();
+  //Triangulos inferiores1
+for(j = 0; j < 2; j++)
+  {
+    for(i = 0; i < 2; i++)
+    {
+      triangle(x1,y1,x1,y2,x2,y2);
+      x1 = x1 + 10;
+      x2 = x2 + 10;
+    }
+    x1 = x1 + 20;
+    x2 = x2 + 20;
+  }
+  
+  //Triangulos superiores1
+  for(j = 0; j < 2; j++)
+  {
+    for(i = 0; i < 2; i++)
+    {
+  triangle(x3,y3,x4,y3,x4,y4);
+      x3 = x3 + 10;
+      x4 = x4 + 10;
+  }
+    x3 = x3 + 20;
+    x4 = x4 + 20;
+  }
+
+  x1= 15;
+  x2= 25;
+  y1= 55;
+  y2= 65;
+  //Triangulos inferiores1
+for(j = 0; j < 2; j++)
+  {
+    for(i = 0; i < 2; i++)
+    {
+      triangle(x1,y1,x1,y2,x2,y2);
+      x1 = x1 + 10;
+      x2 = x2 + 10;
+    }
+    x1 = x1 + 20;
+    x2 = x2 + 20;
+  }
+  
+
+	x3 = 35;
+  x4 = 45;
+  y3 = 55;
+  y4 = 65;
+  //Triangulos superiores1
+  for(j = 0; j < 2; j++)
+  {
+    for(i = 0; i < 2; i++)
+    {
+  triangle(x3,y3,x4,y3,x4,y4);
+      x3 = x3 + 10;
+      x4 = x4 + 10;
+  }
+    x3 = x3 + 20;
+    x4 = x4 + 20;
+  }
+  
+////////////////////////////////////////////////////////////////////////  
+  
+    x1= 35;
+  x2= 45;
+  y1= 35;
+  y2= 45;
+  //Triangulos inferiores3
+for(j = 0; j < 2; j++)
+  {
+    for(i = 0; i < 2; i++)
+    {
+      triangle(x1,y1,x1,y2,x2,y2);
+      x1 = x1 + 10;
+      x2 = x2 + 10;
+    }
+    x1 = x1 + 20;
+    x2 = x2 + 20;
+  }
+  
+
+	x3 = 15;
+  x4 = 25;
+  y3 = 35;
+  y4 = 45;
+  //Triangulos superiores3
+  for(j = 0; j < 2; j++)
+  {
+    for(i = 0; i < 2; i++)
+    {
+  triangle(x3,y3,x4,y3,x4,y4);
+      x3 = x3 + 10;
+      x4 = x4 + 10;
+  }
+    x3 = x3 + 20;
+    x4 = x4 + 20;
+  }
+  
+    x1= 35;
+  x2= 45;
+  y1= 75;
+  y2= 85;
+  //Triangulos inferiores3
+for(j = 0; j < 2; j++)
+  {
+    for(i = 0; i < 2; i++)
+    {
+      triangle(x1,y1,x1,y2,x2,y2);
+      x1 = x1 + 10;
+      x2 = x2 + 10;
+    }
+    x1 = x1 + 20;
+    x2 = x2 + 20;
+  }
+  
+
+	x3 = 15;
+  x4 = 25;
+  y3 = 75;
+  y4 = 85;
+  //Triangulos superiores3
+  for(j = 0; j < 2; j++)
+  {
+    for(i = 0; i < 2; i++)
+    {
+  triangle(x3,y3,x4,y3,x4,y4);
+      x3 = x3 + 10;
+      x4 = x4 + 10;
+  }
+    x3 = x3 + 20;
+    x4 = x4 + 20;
+  }
+//////////////////////////////////////////////////////////////////////// 
+    
+  x1= 25;
+  x2= 35;
+  y1= 45;
+  y2= 55;
+  //Triangulos inferiores4
+for(j = 0; j < 2; j++)
+  {
+    for(i = 0; i < 2; i++)
+    {
+      triangle(x1,y1,x1,y2,x2,y2);
+      x1 = x1 + 10;
+      x2 = x2 + 10;
+    }
+    x1 = x1 + 20;
+    x2 = x2 + 20;
+  }
+  
+
+	x3 = 05;
+  x4 = 15;
+  y3 = 45;
+  y4 = 55;
+  //Triangulos superiores4
+  for(j = 0; j < 2; j++)
+  {
+    for(i = 0; i < 2; i++)
+    {
+  triangle(x3,y3,x4,y3,x4,y4);
+      x3 = x3 + 10;
+      x4 = x4 + 10;
+  }
+    x3 = x3 + 20;
+    x4 = x4 + 20;
+  }
+  
+    x1= 25;
+  x2= 35;
+  y1= 85;
+  y2= 95;
+  //Triangulos inferiores4
+for(j = 0; j < 2; j++)
+  {
+    for(i = 0; i < 2; i++)
+    {
+      triangle(x1,y1,x1,y2,x2,y2);
+      x1 = x1 + 10;
+      x2 = x2 + 10;
+    }
+    x1 = x1 + 20;
+    x2 = x2 + 20;
+  }
+  
+
+	x3 = 05;
+  x4 = 15;
+  y3 = 85;
+  y4 = 95;
+  //Triangulos superiores4
+  for(j = 0; j < 2; j++)
+  {
+    for(i = 0; i < 2; i++)
+    {
+  triangle(x3,y3,x4,y3,x4,y4);
+      x3 = x3 + 10;
+      x4 = x4 + 10;
+  }
+    x3 = x3 + 20;
+    x4 = x4 + 20;
+  }
+//////////////////////////////////////////////////////////////////////// 
+  
+  	x1 = 15;
+		y1 = 25;
+		x2 = 25;
+		y2 = 35;
+  
+  //Triangulos inferiores2
+for(u = 0; u < 2; u++)
+  {
+    for(p = 0; p < 2; p++)
+    {
+      triangle(x1,y1,x1,y2,x2,y2);
+      x1 = x1 + 10;
+      x2 = x2 + 10;
+    }
+    x1 = x1 + 10;
+    x2 = x2 + 10;
+  }
+  
+  	y1 = 25;
+		x2 = 25;
+  	x3 = 35;
+  	y3 = 35;
+  //Triangulos superiores2
+  for(j = 0; j < 2; j++)
+  {
+    for(i = 0; i < 2; i++)
+    {
+  triangle(x2,y1,x3,y1,x3,y3);
+      x2 = x2 + 10;
+      x3 = x3 + 10;
+  }
+    x2 = x2 + 20;
+    x3 = x3 + 20;
+  }
+  {
+    fill(220);
+  triangle(25,25,25,35,35,35)
+  }
+  fill(0);
+    x1 = 15;
+		y1 = 65;
+		x2 = 25;
+		y2 = 75;
+  
+  //Triangulos inferiores2
+for(u = 0; u < 2; u++)
+  {
+    for(p = 0; p < 2; p++)
+    {
+      triangle(x1,y1,x1,y2,x2,y2);
+      x1 = x1 + 10;
+      x2 = x2 + 10;
+    }
+    x1 = x1 + 10;
+    x2 = x2 + 10;
+  }
+  
+  	y1 = 65;
+		x2 = 25;
+  	x3 = 35;
+  	y3 = 75;
+  //Triangulos superiores2
+  for(j = 0; j < 2; j++)
+  {
+    for(i = 0; i < 2; i++)
+    {
+  triangle(x2,y1,x3,y1,x3,y3);
+      x2 = x2 + 10;
+      x3 = x3 + 10;
+  }
+    x2 = x2 + 20;
+    x3 = x3 + 20;
+  }
+  {
+    fill(220);
+  triangle(25,65,25,75,35,75)
+  } 
+////////////////////////////////////////////////////////////////////////
+fill(220);
+  triangle(03,45,15,45,15,56);
+  
+  fill(220);
+  triangle(03,85,15,85,15,96);
+  triangle(03,85,15,85,15,96);
+  fill(0);
+  triangle(85,85,95,85,95,95);
+  triangle(85,45,95,45,95,55);
+  triangle(85,65,85,75,95,75);
+  triangle(85,25,85,35,95,35);
+}
