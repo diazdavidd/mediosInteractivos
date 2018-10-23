b55ab9e2e9cd4e9babb71ef7e88ba825adcc6a98
diazdavidd <42588561+diazdavidd@users.noreply.github.com>
2018-10-23 12:25:33 -0500

    Update sketch.js

--- 09/sketch.js
+++ 09/sketch.js
@@ -12,10 +12,10 @@ function setup() {
   noStroke();
   //configura el texto para que se dibuje con respecto al centro
   textAlign(CENTER);
-  
+
   //inicia las posiciones en el centro de la pantalla
-  x = width/2;
-  y = height-10;
+  x = width / 2;
+  y = height - 10;
 
   //la velocidad inicial es 0
   velx1 = 0;
@@ -23,7 +23,7 @@ function setup() {
 
 }
 //Carga las imágenes usadas en el juego
-function preload(){
+function preload() {
   duque = loadImage('assets/duque.png');
   colombia = loadImage('assets/colombia.png');
 }
@@ -31,78 +31,79 @@ function preload(){
 function draw() {
 
   //Estado 0 que es la pantalla inicial, muestra instrucciones 
-    if (estado == 0) {
-      fill (0);
-      text("Toca la pantalla para comenzar", width / 2, height / 2.5);
-  text("Inclina la pantalla para mover a Colombia", width / 2, height / 2.3);
-  text("¡Salva a Colombia de Uri... Duque!", width / 2, height / 2.12);
-  fill(255, 100);
-	rect(0, 0, width, height);
-	
+  if (estado == 0) {
+    fill(0);
+    text("Toca la pantalla para comenzar", width / 2, height / 2.5);
+    text("Inclina la pantalla para mover a Colombia", width / 2, height / 2.3);
+    text("¡Salva a Colombia de Uri... Duque!", width / 2, height / 2.12);
+    fill(255, 100);
+    rect(0, 0, width, height);
+
   }
-  
+
 
   //Estado 1, acá comienza el juego 
-  else if(estado == 1){
-	fill(255, 220);
-	rect(0, 0, width, height);
-	comenzar();
-    
-    
-  //asigna valores a la velocidad de acuerdo a la rotación del dispositivo
-  velx1 = map(rotationY, -90, 90, -20, 20);
-  vely1 = map(rotationX, -90, 90, -20, 20);
+  else if (estado == 1) {
+    fill(255, 220);
+    rect(0, 0, width, height);
+    comenzar();
 
-  //Dibuja la imagen de Colombia
-  image(colombia, x, y, tam1, tam1);
 
-  //suma la velocidad en x y en y a 
-  x = x + velx1;
-  y = y + vely1;
+    //asigna valores a la velocidad de acuerdo a la rotación del dispositivo
+    velx1 = map(rotationY, -90, 90, -20, 20);
+    vely1 = map(rotationX, -90, 90, -20, 20);
 
-  //evita que las posiciones se salgan del canvas
-  x = constrain(x, tam1/2, width - tam1/2);
-  y = constrain(y, tam1/2, height - tam1/2);
-    
+    //Dibuja la imagen de Colombia
+    image(colombia, x, y, tam1, tam1);
+
+    //suma la velocidad en x y en y a 
+    x = x + velx1;
+    y = y + vely1;
+
+    //evita que las posiciones se salgan del canvas
+    x = constrain(x, tam1 / 2, width - tam1 / 2);
+    y = constrain(y, tam1 / 2, height - tam1 / 2);
+
+
+    //El estado 2 es cuando se gana, aún no se puede ganar pues no he
+    //hecho más niveles
   } else if (estado == 2) {
-		background(220);
+    background(220);
     noStroke();
     fill(0);
-		text("¡Ganaste!", width / 2, height / 2);
+    text("¡Ganaste!", width / 2, height / 2);
 
-	} else if (estado == 3) {
-		background(220);
+    //El estado 3 es cuando se pierde, si toca Duque a Colombia
+  } else if (estado == 3) {
+    background(220);
     noStroke();
     fill(0);
-		text("¡Perdiste!", width / 2, height / 2);
-	}
-  
-  
-  fill(255, 0, 0);
+    text("¡Perdiste!", width / 2, height / 2);
+  }
+
+
 
   //llama a las funciones mostrar, mover y morir de cada uno de los Duques en la lista
   for (var i = 0; i < numDuques; i++) {
     duques[i].mostrar();
     duques[i].mover();
     duques[i].morir();
-        		if (dist(duques[i].x, duques[i].y, x, y) < duques[i].tam) {
-			estado = 3;
-		}
+    if (dist(duques[i].x, duques[i].y, x, y) < duques[i].tam) {
+      estado = 3;
+    }
   }
-  
 
-  
 }
 
 
-//funcion Duque
+//función Duque
 function presidente(px, py) {
 
   //variables de posicion, Duque se crea entre 0 y el ancho de la ventana
   //y entre la posición 0-50 en Y
-  this.x = random(0,windowWidth);
-  this.y = random(0,50);
-  
+  this.x = random(0, windowWidth);
+  this.y = random(0, 50);
+
   //variables de velocidad que recibe la funcion
   this.velx = px;
   this.vely = py;
@@ -111,12 +112,12 @@ function presidente(px, py) {
   this.tam = random(30, 60);
 
 
-  //funcion que dibuja a Duque
+  //función que dibuja a Duque
   this.mostrar = function() {
-    image(duque,this.x, this.y, this.tam, this.tam);
+    image(duque, this.x, this.y, this.tam, this.tam);
   }
 
-  //funcion que mueve a Duque
+  //función que mueve a Duque
   this.mover = function() {
 
     //el movimiento se asigna por la posicion + la velocidad
@@ -144,10 +145,10 @@ function presidente(px, py) {
 }
 
 //funcion que se activa si se acaba el toque en la pantalla
-function touchEnded(){
+function touchEnded() {
   //comenzar el juego cuando se está en el estado inicial
-	if (estado == 0) {
-  	estado = 1;
+  if (estado == 0) {
+    estado = 1;
   }
   //reiniciar el juego cuando ya se perdió
   if (estado == 3) {
